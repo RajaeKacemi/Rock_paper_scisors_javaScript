f2b97dedb29bb750cf651e0506d4bbbf9a17dfb7
Rajaa Kacemi <rajaekacemi@gmail.com>
2023-08-18 11:20:26 +0100

    game is completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A game
+************** ROCK PAPER SCISORS GAME ****************
+
+Today's challenge is to create a simple implementation of the grade-school classic "Rock Paper Scissors" game.
--- index.js
+++ index.js
@@ -1,37 +1,129 @@
-
-function playRound(playerSelection, computerSelection){
-
-    if (playerSelection === "Paper" && computerSelection === "Rock"){
-        console.log("You Win! Paper beats Rock");
-    }
-    else if(playerSelection === "Rock" && computerSelection === "Scissor"){
-        console.log("You Win! Rock beats Scissor");
-    }
-    else if(playerSelection === "Scissor" && computerSelection === "Paper"){
-        console.log("You Win! Scissor beats Paper");
-    }
-    else if (computerSelection === "Paper" && playerSelection === "Rock"){
-        console.log("You Lose! Paper beats Rock");
-    }
-    else if(computerSelection === "Rock" && playerSelection === "Scissor"){
-        console.log("You Lose! Rock beats Scissor");
-    }
-    else if(computerSelection === "Scissor" && playerSelection === "Paper"){
-        console.log("You Lose! Scissor beats Paper");
-    }
-    else{
-        console.log("There is no winner");
-    }
-}
+/**  **/
 function getComputerChoice(){
     var choice = ['Rock','Paper','Scissor'];
     var randomChoice = choice[Math.floor(choice.length * Math.random())];
     return randomChoice;
 }
-var computerChoice = getComputerChoice();
 
-console.log("Computer choice : " + computerChoice);
+game = () => {
 
-let  playerChoice = prompt("Please enter your choice (Rock, Paper, Scissor)\n");
+    let  playerChoice = prompt("Please enter your choice (Rock, Paper, Scissor)\n");
+    console.log("player choice : " + playerChoice);
 
-playRound(playerChoice,computerChoice);
+    var computerChoice = getComputerChoice();
+
+    console.log("Computer choice : " + computerChoice);
+
+    
+    let result = playRound(playerChoice, computerChoice);
+
+    switch(result){
+
+        case "You Win! Paper beats Rock" :
+            return("Player");
+
+        case "You Win! Rock beats Scissor" :
+            return("Player");
+
+        case "You Win! Scissor beats Paper" :
+            return("Player");
+
+        case "You Lose! Paper beats Rock" :
+            return("Computer");
+
+        case "You Lose! Rock beats Scissor" :
+            return("Computer");
+
+        case "You Lose! Scissor beats paper" :
+            return("Computer");
+
+        default : 
+        return("No winner");
+    }
+
+}
+function playRound(playerSelection, computerSelection){
+
+    if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK"){
+        console.log("## You Win! Paper beats Rock ##");
+        return("You Win! Paper beats Rock");
+    }
+    else if(playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSOR"){
+        console.log("## You Win! Rock beats Scissor ##");
+        return("You Win! Rock beats Scissor");  
+    }
+    else if(playerSelection.toUpperCase() === "SCISSOR" && computerSelection.toUpperCase() === "PAPER"){
+        console.log("## You Win! Scissor beats Paper ##");
+        return("You Win! Scissor beats Paper");
+    }
+    else if (computerSelection.toUpperCase() === "PAPER" && playerSelection.toUpperCase()=== "ROCK"){
+        console.log("## You Lose! Paper beats Rock ##");
+        return("You Lose! Paper beats Rock");
+    }
+    else if(computerSelection.toUpperCase() === "ROCK" && playerSelection.toUpperCase() === "SCISSOR"){
+        console.log("## You Lose! Rock beats Scissor ##");
+        return("You Lose! Rock beats Scissor");
+    }
+    else if(computerSelection.toUpperCase() === "SCISSOR" && playerSelection.toUpperCase() === "PAPER"){
+        console.log("## You Lose! Scissor beats paper ##");
+        return("You Lose! Scissor beats paper");
+
+    }
+    else{
+        console.log("## There is no winner ##");
+    }
+}
+
+
+
+let PlayerScoore = 0;
+let ComputerScoore = 0;
+
+let winner = game();
+
+if(winner.toUpperCase() === "PLAYER"){
+    PlayerScoore++;
+}
+else if(winner.toUpperCase() === "COMPUTER"){
+    ComputerScoore++;
+}
+
+let winner2 = game();
+
+if(winner2.toUpperCase() === "PLAYER"){
+    PlayerScoore++;
+}
+else if(winner2.toUpperCase() === "COMPUTER"){
+    ComputerScoore++;
+}
+
+let winner3 = game();
+
+if(winner3.toUpperCase() === "PLAYER"){
+    PlayerScoore++;
+}
+else if(winner3.toUpperCase() === "COMPUTER"){
+    ComputerScoore++;
+}
+
+let winner4 = game();
+
+if(winner4.toUpperCase() === "PLAYER"){
+    PlayerScoore++;
+}
+else if(winner4.toUpperCase() === "COMPUTER"){
+    ComputerScoore++;
+}
+let winner5 = game();
+
+if(winner5.toUpperCase() === "PLAYER"){
+    PlayerScoore++;
+}
+else if(winner5.toUpperCase() === "COMPUTER"){
+    ComputerScoore++;
+}
+
+
+console.log("THE RESULT OF GAME : ** Rock Paper Scisors **");
+console.log("YOUR SCORE : " + PlayerScoore);
+console.log("COMPUTER SCORE : " + ComputerScoore);
